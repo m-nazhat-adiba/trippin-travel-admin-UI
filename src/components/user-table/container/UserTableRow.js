06c8325a212d807c43a08ee2ec0224697047fb14
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-07 12:14:33 +0700

    first commit

---
+++ src/components/user-table/container/UserTableRow.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const UserTableRow = () => {
+  return (
+    <tr className="border-b dark:border-neutral-300">
+      <div className="flex items-center gap-4 px-6 py-4">
+        <img
+          className="inline-block h-12 w-12 p-1 rounded-full ring-2 ring-white"
+          src="/favicon.ico"
+          alt="avatar"
+        />
+        <p>Nazhat Adiba</p>
+      </div>
+      <td className="whitespace-nowrap px-6 py-4">nazhat@dibimbing.id</td>
+      <td className="whitespace-nowrap px-6 py-4">081879856874</td>
+      <td className="whitespace-nowrap px-6 py-4">
+        <p className="bg-red-600 text-white w-fit px-4 py-1 rounded-full font-medium">
+          Admin
+        </p>
+      </td>
+    </tr>
+  );
+};
+
+export default UserTableRow;

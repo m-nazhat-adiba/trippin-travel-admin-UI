e5db16b1453e475e8ff7a44afd129a32e619731e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-07 12:15:38 +0700

    added cards and table components into page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,30 @@
+import ActivityCard from "@/components/cards/ActivityCard";
 import AnalyticCard from "@/components/cards/AnalyticCard";
+import PromoCard from "@/components/cards/PromoCard";
+import UserTable from "@/components/user-table";
 import NavBar from "@/components/navigation/NavBar";
 import SideBar from "@/components/navigation/SideBar";
 
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex w-screen mx-auto">
       <SideBar />
-      <div className="flex flex-col flex-grow items-start px-4">
+      <div className="flex flex-col flex-grow items-start pl-4 pr-8">
         <NavBar />
-        <div className="grid grid-cols-4 w-full gap-8">
-          <AnalyticCard />
-          <AnalyticCard />
-          <AnalyticCard />
-          <AnalyticCard />
+        <div className="flex flex-col w-full gap-5">
+          <div className="grid grid-cols-4 w-full gap-8">
+            <AnalyticCard />
+            <AnalyticCard />
+            <AnalyticCard />
+            <AnalyticCard />
+          </div>
+          <div className="flex gap-6">
+            <ActivityCard />
+            <PromoCard />
+          </div>
+          <div>
+            <UserTable />
+          </div>
         </div>
       </div>
     </main>

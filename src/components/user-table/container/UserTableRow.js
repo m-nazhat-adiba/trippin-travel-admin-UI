56746e235c1823701d1a10adb1d6004ad19ae07e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-07 16:12:01 +0700

    fixed hydration error (expected <td> instead if <div>)

--- src/components/user-table/container/UserTableRow.js
+++ src/components/user-table/container/UserTableRow.js
@@ -3,14 +3,14 @@ import React from "react";
 const UserTableRow = () => {
   return (
     <tr className="border-b dark:border-neutral-300">
-      <div className="flex items-center gap-4 px-6 py-4">
+      <td className="whitespace-nowrap flex items-center gap-2 px-6 py-4">
         <img
           className="inline-block h-12 w-12 p-1 rounded-full ring-2 ring-white"
           src="/favicon.ico"
           alt="avatar"
         />
         <p>Nazhat Adiba</p>
-      </div>
+      </td>
       <td className="whitespace-nowrap px-6 py-4">nazhat@dibimbing.id</td>
       <td className="whitespace-nowrap px-6 py-4">081879856874</td>
       <td className="whitespace-nowrap px-6 py-4">

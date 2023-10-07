9141c386ef4a6c33dd49d0df12657153df76b6c5
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-07 10:38:26 +0700

    imported font Poppins

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }

3586ee6667d23d84031bec805e2083857c733295
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:55:12 +0700

    edited breadcrumbs and title so it will changes based on route pathName

--- src/components/common/navigation/NavBar.js
+++ src/components/common/navigation/NavBar.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
+  const router = useRouter();
+  const path = router.pathname;
   return (
     <div className="flex justify-between w-full items-center px-5 py-8">
       <div className="flex flex-col">
         <p className="font-normal">
-          Pages / <span className="font-medium">Dashboard</span>
+          Pages / {""}
+          <span className="font-medium">
+            {path !== "/"
+              ? path.charAt(1).toUpperCase() + path.slice(2)
+              : "Dashboard"}
+          </span>
         </p>
-        <h1 className="font-bold">Dashboard</h1>
+        <h1 className="font-bold">
+          {path !== "/"
+            ? path.charAt(1).toUpperCase() + path.slice(2)
+            : "Dashboard"}
+        </h1>
       </div>
       <div>
         <Icon icon="material-symbols:person" className="text-xl" />

689aedac39a121f7f23cfd7c10d4c859b6c6b648
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:21:31 +0700

    added props

--- src/components/dashboard/user-table/container/UserTableRow.js
+++ src/components/dashboard/user-table/container/UserTableRow.js
@@ -1,21 +1,27 @@
+import clsx from "clsx";
 import React from "react";
 
-const UserTableRow = () => {
+const UserTableRow = ({ data }) => {
   return (
     <tr className="border-b dark:border-neutral-300">
       <td className="whitespace-nowrap flex items-center gap-2 px-6 py-4">
         <img
           className="inline-block h-12 w-12 p-1 rounded-full ring-2 ring-white"
-          src="/favicon.ico"
+          src={data.profilePictureUrl}
           alt="avatar"
         />
-        <p>Nazhat Adiba</p>
+        <p>{data.name}</p>
       </td>
-      <td className="whitespace-nowrap px-6 py-4">nazhat@dibimbing.id</td>
-      <td className="whitespace-nowrap px-6 py-4">081879856874</td>
+      <td className="whitespace-nowrap px-6 py-4">{data.email}</td>
+      <td className="whitespace-nowrap px-6 py-4">{data.phoneNumber}</td>
       <td className="whitespace-nowrap px-6 py-4">
-        <p className="bg-red-600 text-white w-fit px-4 py-1 rounded-full font-medium">
-          Admin
+        <p
+          className={clsx(
+            " text-white w-fit px-4 py-1 rounded-full font-medium",
+            data.role === "admin" ? "bg-red-600" : "bg-green-600"
+          )}
+        >
+          {data.role}
         </p>
       </td>
     </tr>

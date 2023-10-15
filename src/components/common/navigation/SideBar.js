2b6fd30dab548ab21c1f9553ec45755697250376
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:45:18 +0700

    added route hook and clsx function for tab button

--- src/components/common/navigation/SideBar.js
+++ src/components/common/navigation/SideBar.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import React from "react";
-import { Icon } from "@iconify/react";
 import Link from "next/link";
+import clsx from "clsx";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/router";
 
 const SideBar = () => {
+  const router = useRouter();
+  const path = router.pathname;
   return (
     <div className="flex flex-col h-screen w-[298px] px-8 gap-6">
       <Image
@@ -15,49 +19,138 @@ const SideBar = () => {
       />
       <div className="flex flex-col gap-1">
         <Link href="/">
-          <button className="flex items-center gap-2 w-52 h-14 bg-white px-4 rounded-xl shadow-md">
+          <button
+            className={clsx(
+              "flex items-center gap-2 w-52 h-14 px-4 rounded-xl ",
+              path === "/" ? " bg-white shadow-md" : ""
+            )}
+          >
             <Icon
               icon="material-symbols:home"
-              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+              className={clsx(
+                "w-8 h-8 p-2  rounded-xl",
+                path === "/"
+                  ? "bg-[#4FD1C5] text-white"
+                  : "text-[#4FD1C5] bg-white shadow-md"
+              )}
             />
-            <p className="font-bold">Dashboard</p>
+            <p
+              className={clsx(
+                "font-bold",
+                path === "/" ? "text-gray-900" : "text-gray-400"
+              )}
+            >
+              Dashboard
+            </p>
           </button>
         </Link>
+
         <Link href="/activities">
-          <button className="flex items-center gap-2 w-52 h-14 px-4">
+          <button
+            className={clsx(
+              "flex items-center gap-2 w-52 h-14 px-4 rounded-xl ",
+              path.includes("/activities") ? " bg-white shadow-md" : ""
+            )}
+          >
             <Icon
               icon="material-symbols:flight"
               rotate={1}
-              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+              className={clsx(
+                "w-8 h-8 p-2  rounded-xl",
+                path.includes("/activities")
+                  ? "bg-[#4FD1C5] text-white"
+                  : "text-[#4FD1C5] bg-white shadow-md"
+              )}
             />
-            <p className="font-bold">Activities</p>
+            <p
+              className={clsx(
+                "font-bold",
+                path.includes("/activities") ? "text-gray-900" : "text-gray-400"
+              )}
+            >
+              Activities
+            </p>
           </button>
         </Link>
+
         <Link href="/users">
-          <button className="flex items-center gap-2 w-52 h-14  px-4">
+          <button
+            className={clsx(
+              "flex items-center gap-2 w-52 h-14 px-4 rounded-xl ",
+              path.includes("/users") ? " bg-white shadow-md" : ""
+            )}
+          >
             <Icon
               icon="material-symbols:person-search-rounded"
-              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+              className={clsx(
+                "w-8 h-8 p-2  rounded-xl",
+                path.includes("/users")
+                  ? "bg-[#4FD1C5] text-white"
+                  : "text-[#4FD1C5] bg-white shadow-md"
+              )}
             />
-            <p className="font-bold">Users</p>
+            <p
+              className={clsx(
+                "font-bold",
+                path.includes("/users") ? "text-gray-900" : "text-gray-400"
+              )}
+            >
+              Users
+            </p>
           </button>
         </Link>
+
         <Link href="/promos">
-          <button className="flex items-center gap-2 w-52 h-14 b px-4">
+          <button
+            className={clsx(
+              "flex items-center gap-2 w-52 h-14 px-4 rounded-xl ",
+              path.includes("/promos") ? " bg-white shadow-md" : ""
+            )}
+          >
             <Icon
               icon="material-symbols:local-activity-rounded"
-              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+              className={clsx(
+                "w-8 h-8 p-2  rounded-xl",
+                path.includes("/promos")
+                  ? "bg-[#4FD1C5] text-white"
+                  : "text-[#4FD1C5] bg-white shadow-md"
+              )}
             />
-            <p className="font-bold">Promos</p>
+            <p
+              className={clsx(
+                "font-bold",
+                path.includes("/promos") ? "text-gray-900" : "text-gray-400"
+              )}
+            >
+              Promos
+            </p>
           </button>
         </Link>
+
         <Link href="/categories">
-          <button className="flex items-center gap-2 w-52 h-14  px-4">
+          <button
+            className={clsx(
+              "flex items-center gap-2 w-52 h-14 px-4 rounded-xl ",
+              path.includes("/categories") ? " bg-white shadow-md" : ""
+            )}
+          >
             <Icon
               icon="material-symbols:filter-alt-sharp"
-              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+              className={clsx(
+                "w-8 h-8 p-2  rounded-xl",
+                path.includes("/categories")
+                  ? "bg-[#4FD1C5] text-white"
+                  : "text-[#4FD1C5] bg-white shadow-md"
+              )}
             />
-            <p className="font-bold">Categories</p>
+            <p
+              className={clsx(
+                "font-bold",
+                path.includes("/categories") ? "text-gray-900" : "text-gray-400"
+              )}
+            >
+              Categories
+            </p>
           </button>
         </Link>
       </div>

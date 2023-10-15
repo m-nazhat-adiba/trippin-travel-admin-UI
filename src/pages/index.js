7993cd54a44328fac9b886e6a7ffd17aad86be6e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:22:33 +0700

    added Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,23 +4,74 @@ import PromoCard from "@/components/dashboard/cards/PromoCard";
 import UserTable from "@/components/dashboard/user-table";
 import Layout from "@/components/common/layout";
 import React from "react";
+import useAxios from "@/hooks/useAxios";
+import { activityConfig, userConfig } from "@/constant/config";
 
 const Home = () => {
+  const userData = useAxios(
+    "https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/all-user",
+    userConfig
+  );
+  const activityData = useAxios(
+    "https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/activities",
+    activityConfig
+  );
+
   return (
     <Layout>
       <div className="flex flex-col w-full gap-5">
         <div className="grid grid-cols-4 w-full gap-8">
-          <AnalyticCard />
-          <AnalyticCard />
-          <AnalyticCard />
-          <AnalyticCard />
+          {activityData.loading ? (
+            <p>Loading...</p>
+          ) : activityData.error ? (
+            <p>Error: {activityData.error.message}</p>
+          ) : (
+            <AnalyticCard data={activityData.data.data} title="Activity Data" />
+          )}
+
+          {userData.loading ? (
+            <p>Loading...</p>
+          ) : userData.error ? (
+            <p>Error: {userData.error.message}</p>
+          ) : (
+            <AnalyticCard data={userData.data.data} title="Users Data" />
+          )}
+
+          {userData.loading ? (
+            <p>Loading...</p>
+          ) : userData.error ? (
+            <p>Error: {userData.error.message}</p>
+          ) : (
+            <AnalyticCard data={userData.data.data} />
+          )}
+
+          {userData.loading ? (
+            <p>Loading...</p>
+          ) : userData.error ? (
+            <p>Error: {userData.error.message}</p>
+          ) : (
+            <AnalyticCard data={userData.data.data} />
+          )}
         </div>
+
         <div className="flex gap-6">
-          <ActivityCard />
+          {activityData.loading ? (
+            <p>Loading...</p>
+          ) : activityData.error ? (
+            <p>Error: {activityData.error.message}</p>
+          ) : (
+            <ActivityCard activityData={activityData.data.data[0]} />
+          )}
           <PromoCard />
         </div>
         <div>
-          <UserTable />
+          {userData.loading ? (
+            <p>Loading...</p>
+          ) : userData.error ? (
+            <p>Error: {userData.error.message}</p>
+          ) : (
+            <UserTable userData={userData.data} />
+          )}
         </div>
       </div>
     </Layout>

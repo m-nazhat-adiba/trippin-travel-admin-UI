d259f1025a76e4e9fba8bf67341fa183232be6c6
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:23:13 +0700

    first commit

---
+++ src/constant/config.js
@@ -0,0 +1,16 @@
+const API_KEY = "24405e01-fbc1-45a5-9f5a-be13afcd757c";
+const JWT_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pZnRhaGZhcmhhbkBnbWFpbC5jb20iLCJ1c2VySWQiOiI5NWE4MDNjMy1iNTFlLTQ3YTAtOTBkYi0yYzJmM2Y0ODE1YTkiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2Nzk4NDM0NDR9.ETsN6dCiC7isPReiQyHCQxya7wzj05wz5zruiFXLx0k";
+
+export const userConfig = {
+  headers: {
+    apiKey: API_KEY,
+    Authorization: `Bearer ${JWT_TOKEN}`,
+  },
+};
+
+export const activityConfig = {
+  headers: {
+    apiKey: API_KEY,
+  },
+};
---
+++ src/hooks/useAxios.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useAxios(url, config) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Axios GET request
+    axios
+      .get(url, config)
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useAxios;

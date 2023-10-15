20e0fec2c3dbf05ebbb32754721ec6e5e2ba581a
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:20:09 +0700

    encapsulated button tab with Link component

--- src/components/common/navigation/SideBar.js
+++ src/components/common/navigation/SideBar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 const SideBar = () => {
   return (
@@ -13,42 +14,52 @@ const SideBar = () => {
         className="my-8 pt-3"
       />
       <div className="flex flex-col gap-1">
-        <button className="flex items-center gap-2 w-52 h-14 bg-white px-4 rounded-xl shadow-md">
-          <Icon
-            icon="material-symbols:home"
-            className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
-          />
-          <p className="font-bold">Dashboard</p>
-        </button>
-        <button className="flex items-center gap-2 w-52 h-14 px-4">
-          <Icon
-            icon="material-symbols:flight"
-            rotate={1}
-            className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
-          />
-          <p className="font-bold">Activities</p>
-        </button>
-        <button className="flex items-center gap-2 w-52 h-14  px-4">
-          <Icon
-            icon="material-symbols:person-search-rounded"
-            className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
-          />
-          <p className="font-bold">Users</p>
-        </button>
-        <button className="flex items-center gap-2 w-52 h-14 b px-4">
-          <Icon
-            icon="material-symbols:local-activity-rounded"
-            className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
-          />
-          <p className="font-bold">Promos</p>
-        </button>
-        <button className="flex items-center gap-2 w-52 h-14  px-4">
-          <Icon
-            icon="material-symbols:filter-alt-sharp"
-            className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
-          />
-          <p className="font-bold">Categories</p>
-        </button>
+        <Link href="/">
+          <button className="flex items-center gap-2 w-52 h-14 bg-white px-4 rounded-xl shadow-md">
+            <Icon
+              icon="material-symbols:home"
+              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+            />
+            <p className="font-bold">Dashboard</p>
+          </button>
+        </Link>
+        <Link href="/activities">
+          <button className="flex items-center gap-2 w-52 h-14 px-4">
+            <Icon
+              icon="material-symbols:flight"
+              rotate={1}
+              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+            />
+            <p className="font-bold">Activities</p>
+          </button>
+        </Link>
+        <Link href="/users">
+          <button className="flex items-center gap-2 w-52 h-14  px-4">
+            <Icon
+              icon="material-symbols:person-search-rounded"
+              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+            />
+            <p className="font-bold">Users</p>
+          </button>
+        </Link>
+        <Link href="/promos">
+          <button className="flex items-center gap-2 w-52 h-14 b px-4">
+            <Icon
+              icon="material-symbols:local-activity-rounded"
+              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+            />
+            <p className="font-bold">Promos</p>
+          </button>
+        </Link>
+        <Link href="/categories">
+          <button className="flex items-center gap-2 w-52 h-14  px-4">
+            <Icon
+              icon="material-symbols:filter-alt-sharp"
+              className="w-8 h-8 p-2 bg-[#4FD1C5] text-white rounded-xl"
+            />
+            <p className="font-bold">Categories</p>
+          </button>
+        </Link>
       </div>
     </div>
   );

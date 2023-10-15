873c7c00a8bb19b010556d6e50fa06fd4105ac8e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-15 08:20:48 +0700

    added props

--- src/components/dashboard/cards/ActivityCard.js
+++ src/components/dashboard/cards/ActivityCard.js
@@ -1,18 +1,25 @@
 import Image from "next/image";
 import React from "react";
 
-const ActivityCard = ({ category = "Mountain", title = "Hiking" }) => {
+const ActivityCard = ({ activityData }) => {
   return (
-    <article className="flex bg-white shadow-md px-5 py-4 max-w-[920px] rounded-lg">
-      <div className="flex flex-col">
-        <h2 className="font-bold text-gray-500 text-xs">{category}</h2>
-        <h1 className="text-xl font-bold text-gray-900">{title}</h1>
-        <p className="w-1/2 py-2">
-          From colors, cards, typography to complex elements, you will find the
-          full documentation.
-        </p>
+    <article className="flex bg-white shadow-md px-5 py-4 w-[60%] rounded-lg">
+      <div className="flex flex-col w-full">
+        <h2 className="font-bold text-gray-500 text-xs">
+          {activityData.category.name}
+        </h2>
+        <h1 className="text-xl font-bold text-gray-900">
+          {activityData.title}
+        </h1>
+        <p className="w-1/2 py-2">{activityData.description}</p>
       </div>
-      <Image src="/activity.png" width={360} height={256} alt="activity" />
+      <Image
+        src={activityData.imageUrls[0]}
+        width={360}
+        height={256}
+        alt="activity"
+        className="rounded-md"
+      />
     </article>
   );
 };

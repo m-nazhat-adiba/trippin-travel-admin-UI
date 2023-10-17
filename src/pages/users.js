81efafcd2f5deccc3685f44e5d207f0af156279a
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-17 23:12:33 +0700

    added component layout component

--- src/pages/users.js
+++ src/pages/users.js
@@ -4,18 +4,21 @@ import { USER_CONFIG } from "@/constant/config";
 import useAxios from "@/hooks/useAxios";
 import React from "react";
 import UserTable from "@/components/common/table/users";
+import ContentLayout from "@/components/common/layout/content";
 
 const Users = () => {
   const userData = useAxios(USERS.GET_ALL_USERS, USER_CONFIG);
   return (
     <Layout>
-      {userData.loading ? (
-        <p>Loading...</p>
-      ) : userData.error ? (
-        <p>Error: {userData.error.message}</p>
-      ) : (
-        <UserTable userData={userData.data} />
-      )}
+      <ContentLayout title="All Users" sub="Manage users" button="User">
+        {userData.loading ? (
+          <p>Loading...</p>
+        ) : userData.error ? (
+          <p>Error: {userData.error.message}</p>
+        ) : (
+          <UserTable userData={userData.data} />
+        )}
+      </ContentLayout>
     </Layout>
   );
 };

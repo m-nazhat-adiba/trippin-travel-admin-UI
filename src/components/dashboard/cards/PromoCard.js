4addda74e19da975df03a72353198234bc96b9f2
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-17 23:09:59 +0700

    renamed

--- src/components/dashboard/cards/PromoCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PromoCard = () => {
-  return (
-    <article className="bg-[url('/promo-bg.png')] w-[654px] h-[290px] overflow-hidden border-[16px] border-white shadow-md rounded-lg"></article>
-  );
-};
-
-export default PromoCard;

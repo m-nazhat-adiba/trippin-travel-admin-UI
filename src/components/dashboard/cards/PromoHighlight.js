fcf0bea0b01fc478d3f39220dae4021ac65ef105
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-17 23:10:25 +0700

    renamed

---
+++ src/components/dashboard/cards/PromoHighlight.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PromoHighlight = () => {
+  return (
+    <article className="bg-[url('/promo-bg.png')] w-[654px] h-[290px] overflow-hidden border-[16px] border-white shadow-md rounded-lg"></article>
+  );
+};
+
+export default PromoHighlight;

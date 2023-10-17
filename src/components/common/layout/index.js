82c13f273a5510e29e9ab1537882ce2bc4d09ea1
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-17 23:05:11 +0700

    edited width

--- src/components/common/layout/index.js
+++ src/components/common/layout/index.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex w-screen mx-auto">
       <SideBar />
-      <div className="flex flex-col flex-grow items-start pl-4 pr-8">
+      <div className="flex flex-col w-full items-start pl-4 pr-8">
         <NavBar />
         {children}
       </div>

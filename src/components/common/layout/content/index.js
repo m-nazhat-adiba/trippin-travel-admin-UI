886f6b627ce6e9b7500f4077f0a391a4691745a0
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-17 23:05:35 +0700

    first commit

---
+++ src/components/common/layout/content/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+const ContentLayout = ({ children, button, title, sub }) => {
+  return (
+    <div className="w-full flex flex-col bg-white shadow-md py-4 rounded-lg gap-3">
+      <header className="flex w-full justify-between px-5 ">
+        <div className="flex flex-col">
+          <h3 className="font-bold">{title}</h3>
+          <p className="text-gray-500 text-sm">{sub}</p>
+        </div>
+        <button className="flex gap-2 items-center px-3">
+          <Icon icon="akar-icons:plus" className="text-lg" />
+          <p>Create a New {button}</p>
+        </button>
+      </header>
+      {children}
+    </div>
+  );
+};
+
+export default ContentLayout;

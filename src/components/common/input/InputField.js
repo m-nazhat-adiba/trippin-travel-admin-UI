dff6eec530eb4abbedba956f69c3e2f871af4cc0
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-25 14:46:35 +0700

    added hook prop

--- src/components/common/input/InputField.js
+++ src/components/common/input/InputField.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const InputField = ({ label = "Label", placeholder = "Placeholder" }) => {
+const InputField = ({
+  label = "Label",
+  placeholder = "Placeholder",
+  inputHook,
+}) => {
+  const inputProps = inputHook;
+
   return (
     <div className="flex flex-col gap-1">
       <label>{label}</label>
       <input
         placeholder={placeholder}
+        {...inputProps}
         className="border-2 border-gray-200 rounded-xl px-2 py-3"
       />
     </div>

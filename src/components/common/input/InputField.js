2df76f2e63d435c4def738596298c714535badab
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-26 23:13:29 +0700

    added props

--- src/components/common/input/InputField.js
+++ src/components/common/input/InputField.js
@@ -4,17 +4,20 @@ const InputField = ({
   label = "Label",
   placeholder = "Placeholder",
   inputHook,
+  className,
 }) => {
   const inputProps = inputHook;
 
   return (
-    <div className="flex flex-col gap-1">
-      <label>{label}</label>
-      <input
-        placeholder={placeholder}
-        {...inputProps}
-        className="border-2 border-gray-200 rounded-xl px-2 py-3"
-      />
+    <div className={`${className}`}>
+      <div className="flex flex-col gap-1">
+        <label>{label}</label>
+        <input
+          placeholder={placeholder}
+          {...inputProps}
+          className={`border-2 border-gray-300 rounded-xl px-2 py-3`}
+        />
+      </div>
     </div>
   );
 };

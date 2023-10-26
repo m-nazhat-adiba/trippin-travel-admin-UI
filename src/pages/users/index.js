871a31e304817035ffba202bf1e5a431a515cd8b
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-26 23:15:57 +0700

    created handleEditModal function

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,11 +1,18 @@
 import Layout from "@/components/common/layout";
-import React from "react";
+import React, { useState } from "react";
 import UserTable from "@/components/common/table/users";
 import ContentLayout from "@/components/common/layout/content";
 import { userService } from "@/apis";
+import Modal from "@/components/common/modal";
+import InputField from "@/components/common/input/InputField";
 
 const Users = () => {
   const userData = userService.getUserList();
+  const [showModal, setShowModal] = useState(false);
+  const handleEditModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <Layout>
       <ContentLayout title="All Users" sub="Manage users">
@@ -14,8 +21,17 @@ const Users = () => {
         ) : userData.error ? (
           <p>Error: {userData.error.message}</p>
         ) : (
-          <UserTable userData={userData.data} />
+          <UserTable handleModal={handleEditModal} userData={userData.data} />
         )}
+        {showModal ? (
+          <Modal title="Edit User Role" handleModal={handleEditModal}>
+            <InputField
+              className="w-96"
+              placeholder="admin, user"
+              label="Role"
+            />
+          </Modal>
+        ) : null}
       </ContentLayout>
     </Layout>
   );

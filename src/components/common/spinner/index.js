05bb649fba6c5cb09f25ac737520435e1a9faa20
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-28 08:24:45 +0700

    edited width constrain

--- src/components/common/spinner/index.js
+++ src/components/common/spinner/index.js
@@ -5,7 +5,7 @@ const Spinner = ({ className }) => {
     <div role="status">
       <svg
         aria-hidden="true"
-        class={`inline ${className} mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300`}
+        className={`inline ${className} mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"

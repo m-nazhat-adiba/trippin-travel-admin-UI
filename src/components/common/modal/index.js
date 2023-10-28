67fbc4a89f92fb54a5953d6bfaaaebf1b588baeb
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-28 09:09:42 +0700

    added condition prop

--- src/components/common/modal/index.js
+++ src/components/common/modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Modal = ({ handleModal, children, title }) => {
+const Modal = ({ handleModal, children, title, submit = false }) => {
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -30,13 +30,15 @@ const Modal = ({ handleModal, children, title }) => {
               >
                 Close
               </button>
-              <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={handleModal}
-              >
-                Save
-              </button>
+              {submit ? (
+                <button
+                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={handleModal}
+                >
+                  Save
+                </button>
+              ) : null}
             </div>
           </div>
         </div>

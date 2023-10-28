08e55898a1314419a88591233fbfd9b4c37208be
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-29 04:02:51 +0700

    added delete api call and Link

--- src/components/category/card/Category.js
+++ src/components/category/card/Category.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import imageLoader from "@/utils/imageLoader";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { deleteData } from "@/utils/fetchData";
+import { CATEGORIES } from "@/constant/api";
+import { USER_CONFIG } from "@/constant/config";
 
 const Category = ({ data }) => {
+  const [response, setResponse] = useState();
+  const [error, setError] = useState();
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      const deletePost = await deleteData(
+        CATEGORIES.DELETE + data.id,
+        USER_CONFIG
+      );
+      setResponse(deletePost);
+      router.reload();
+    } catch (error) {
+      setError(error);
+    }
+  };
   return (
     <div className="flex flex-col gap-5 mb-4">
       <Image
@@ -18,10 +39,15 @@ const Category = ({ data }) => {
           <h2 className="font-bold text-xl">{data.name}</h2>
         </div>
         <div className="flex gap-2 mt-3">
-          <button className="py-2 px-6 bg-[#4FD1C5] rounded-lg text-white font-bold">
-            Edit
-          </button>
-          <button className="py-2 px-4 border-2 border-gray-300 rounded-lg">
+          <Link href={`/categories/${data.id}`}>
+            <button className="py-2 px-6 bg-[#4FD1C5] rounded-lg text-white font-bold">
+              Edit
+            </button>
+          </Link>
+          <button
+            onClick={handleDelete}
+            className="py-2 px-4 border-2 border-gray-300 rounded-lg"
+          >
             Delete
           </button>
         </div>

6778d11129d5f75f4ec4c3dd9ce79eb274fb392a
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-28 22:16:42 +0700

    added state to capture id and API calling fn

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,17 +1,57 @@
 import Layout from "@/components/common/layout";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserTable from "@/components/common/table/users";
 import ContentLayout from "@/components/common/layout/content";
 import { userService } from "@/apis";
 import Modal from "@/components/common/modal";
 import InputField from "@/components/common/input/InputField";
+import useInput from "@/hooks/useInput";
+import { postData } from "@/utils/fetchData";
+import { USERS } from "@/constant/api";
+import SelectInput from "@/components/common/input/SelectInput";
+import { USER_CONFIG } from "@/constant/config";
 
 const Users = () => {
   const userData = userService.getUserList();
+  const roleHook = useInput();
   const [showModal, setShowModal] = useState(false);
+  const [dataPost, setDataPost] = useState();
+  const [loadingPost, setLoadingPost] = useState(false);
+  const [errorPost, setErrorPost] = useState(null);
+  const [userId, setUserId] = useState();
+
+  const roles = [
+    { id: "admin", name: "Admin" },
+    { id: "user", name: "User" },
+  ];
+
   const handleEditModal = () => {
     setShowModal(!showModal);
   };
+  const handleChangeRole = async (e) => {
+    e.preventDefault();
+    const payload = {
+      role: roleHook.data,
+    };
+    try {
+      const result = await postData(
+        USERS.UPDATE_ROLE + userId,
+        payload,
+        USER_CONFIG
+      );
+      setDataPost(result.data);
+    } catch (error) {
+      setErrorPost(error);
+      console.log(errorPost);
+    } finally {
+      setLoadingPost(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log("id", userId);
+    console.log("role", roleHook.data);
+  }, [userId, roleHook.data]);
 
   return (
     <Layout>
@@ -21,15 +61,20 @@ const Users = () => {
         ) : userData.error ? (
           <p>Error: {userData.error.message}</p>
         ) : (
-          <UserTable handleModal={handleEditModal} userData={userData.data} />
+          <UserTable
+            setData={setUserId}
+            handleModal={handleEditModal}
+            userData={userData.data}
+          />
         )}
         {showModal ? (
-          <Modal title="Edit User Role" handleModal={handleEditModal}>
-            <InputField
-              className="w-96"
-              placeholder="admin, user"
-              label="Role"
-            />
+          <Modal
+            submit={true}
+            title="Edit User Role"
+            handleModal={handleEditModal}
+            handleSubmit={handleChangeRole}
+          >
+            <SelectInput inputHook={roleHook} data={roles} />
           </Modal>
         ) : null}
       </ContentLayout>

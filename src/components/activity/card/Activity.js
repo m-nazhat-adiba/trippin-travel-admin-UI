906191a74b394716cf86c94513c55e756b433319
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-28 09:09:18 +0700

    added delete api call and prop for notif modal

--- src/components/activity/card/Activity.js
+++ src/components/activity/card/Activity.js
@@ -1,9 +1,27 @@
 import imageLoader from "@/utils/imageLoader";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { ACTIVITIES } from "@/constant/api";
+import { USER_CONFIG } from "@/constant/config";
+import { deleteData } from "@/utils/fetchData";
 
-const Activity = ({ data }) => {
+const Activity = ({ data, showModal }) => {
+  const [response, setResponse] = useState();
+  const [error, setError] = useState();
+  const handleDelete = async () => {
+    try {
+      const deletePost = await deleteData(
+        ACTIVITIES.DELETE + data.id,
+        USER_CONFIG
+      );
+      setResponse(deletePost);
+      showModal(true);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <div className="flex px-5 py-4 w-full">
       <div className="flex flex-col w-full gap-2">
@@ -40,7 +58,10 @@ const Activity = ({ data }) => {
               Edit
             </button>
           </Link>
-          <button className="py-2 px-4 border-2 border-gray-300 rounded-lg">
+          <button
+            onClick={handleDelete}
+            className="py-2 px-4 border-2 border-gray-300 rounded-lg"
+          >
             Delete
           </button>
         </div>

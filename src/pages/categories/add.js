58fa4c34a60c0ccc6f051f70f6a9492889e46494
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-29 04:03:27 +0700

    added post api call and link

--- src/pages/categories/add.js
+++ src/pages/categories/add.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@/components/common/layout";
 import ContentLayout from "@/components/common/layout/content";
 import InputField from "@/components/common/input/InputField";
 import useInput from "@/hooks/useInput";
+import { postData } from "@/utils/fetchData";
+import { CATEGORIES } from "@/constant/api";
+import { USER_CONFIG } from "@/constant/config";
+import Link from "next/link";
+import Modal from "@/components/common/modal";
 
 const AddCategory = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const [dataPost, setDataPost] = useState(null);
+  const [loadingPost, setLoadingPost] = useState(false);
+  const [errorPost, setErrorPost] = useState(null);
+
   const titleHook = useInput();
   const imageUrlHook = useInput();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoadingPost(true);
+    const payload = {
+      name: titleHook.data,
+      imageUrl: imageUrlHook.data,
+    };
+    try {
+      const result = await postData(CATEGORIES.CREATE, payload, USER_CONFIG);
+      setDataPost(result.data);
+      setShowModal(true);
+    } catch (error) {
+      setErrorPost(error);
+      console.log(error);
+    } finally {
+      setLoadingPost(false);
+    }
+  };
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
 
   return (
     <Layout>
       <ContentLayout title="Add New Category">
-        <form className="flex flex-col gap-9">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-9">
           <div className="flex flex-col gap-7 my-2">
             <div className="grid grid-cols-4 gap-7 w-full px-5">
               <InputField
@@ -37,11 +70,21 @@ const AddCategory = () => {
             >
               Save
             </button>
-            <button className="py-2 px-4 border-2 border-gray-300 rounded-lg">
-              Cancel
-            </button>
+            <Link href={"/categories"}>
+              <button
+                type="button"
+                className="py-2 px-4 border-2 border-gray-300 rounded-lg"
+              >
+                Cancel
+              </button>
+            </Link>
           </div>
         </form>
+        {showModal ? (
+          <Modal handleModal={handleModal} title="Status">
+            <p>Success</p>
+          </Modal>
+        ) : null}
       </ContentLayout>
     </Layout>
   );

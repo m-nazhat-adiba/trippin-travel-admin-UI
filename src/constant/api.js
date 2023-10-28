aa758a64cdc8083420f03eadba37ee297871b43e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-28 08:25:07 +0700

    added new endpoint

--- src/constant/api.js
+++ src/constant/api.js
@@ -4,6 +4,8 @@ export const USERS = { GET_ALL_USERS: `${BASE_URL}/api/v1/all-user` };
 
 export const ACTIVITIES = {
   GET_ALL_ACTIVITIES: `${BASE_URL}/api/v1/activities`,
+  GET_BY_ID: `${BASE_URL}/api/v1/activity/`,
+  UPDATE: `${BASE_URL}/api/v1/update-activity/`,
 };
 
 export const PROMOS = { GET_ALL_PROMOS: `${BASE_URL}/api/v1/promos` };

e2d0d14c7018f6da39424dd267c97e21cb6b58a1
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-28 22:11:00 +0700

    added new endpoint

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,15 +1,22 @@
 const BASE_URL = "https://travel-journal-api-bootcamp.do.dibimbing.id";
 
-export const USERS = { GET_ALL_USERS: `${BASE_URL}/api/v1/all-user` };
+export const USERS = {
+  GET_ALL_USERS: `${BASE_URL}/api/v1/all-user`,
+  UPDATE_ROLE: `${BASE_URL}/api/v1/update-user-role/`,
+};
 
 export const ACTIVITIES = {
   GET_ALL_ACTIVITIES: `${BASE_URL}/api/v1/activities`,
   GET_BY_ID: `${BASE_URL}/api/v1/activity/`,
   UPDATE: `${BASE_URL}/api/v1/update-activity/`,
   DELETE: `${BASE_URL}/api/v1/delete-activity/`,
+  CREATE: `${BASE_URL}/api/v1/create-activity`,
 };
 
-export const PROMOS = { GET_ALL_PROMOS: `${BASE_URL}/api/v1/promos` };
+export const PROMOS = {
+  GET_ALL_PROMOS: `${BASE_URL}/api/v1/promos`,
+  CREATE: `${BASE_URL}/api/v1/create-promo`,
+};
 
 export const CATEGORIES = {
   GET_ALL_CATEGORES: `${BASE_URL}/api/v1/categories`,

179856c06d4ed2ebe070221611a39557b6804bbf
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-29 03:38:31 +0700

    added delete api call and Link

--- src/components/promos/card/PromoCard.js
+++ src/components/promos/card/PromoCard.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import imageLoader from "@/utils/imageLoader";
+import Link from "next/link";
+import { deleteData } from "@/utils/fetchData";
+import { PROMOS } from "@/constant/api";
+import { USER_CONFIG } from "@/constant/config";
+import { useRouter } from "next/router";
 
 const PromoCard = ({ data }) => {
+  const [response, setResponse] = useState();
+  const [error, setError] = useState();
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      const deletePost = await deleteData(PROMOS.DELETE + data.id, USER_CONFIG);
+      setResponse(deletePost);
+      router.reload();
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-5 mb-4">
       <Image
@@ -28,10 +47,15 @@ const PromoCard = ({ data }) => {
           <li>{data.terms_condition}</li>
         </ul>
         <div className="flex gap-2 mt-3">
-          <button className="py-2 px-6 bg-[#4FD1C5] rounded-lg text-white font-bold">
-            Edit
-          </button>
-          <button className="py-2 px-4 border-2 border-gray-300 rounded-lg">
+          <Link href={`/promos/${data.id}`}>
+            <button className="py-2 px-6 bg-[#4FD1C5] rounded-lg text-white font-bold">
+              Edit
+            </button>
+          </Link>
+          <button
+            onClick={handleDelete}
+            className="py-2 px-4 border-2 border-gray-300 rounded-lg"
+          >
             Delete
           </button>
         </div>

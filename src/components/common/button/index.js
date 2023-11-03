e4173f2e23b7647f352e3aaec457a9ff201fd031
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-03 09:26:59 +0700

    added new variant

--- src/components/common/button/index.js
+++ src/components/common/button/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 const VARIANTS = {
-  primary: "bg-[#4FD1C5] text-white font-bold",
-  secondary: "border-2 border-gray-400 bg-white",
+  primary: "bg-[#4FD1C5] text-white font-bold rounded-lg",
+  secondary: "border-2 border-gray-400 bg-white rounded-lg",
+  rounded:
+    "border-2 border-gray-300 w-1/2 px-6 py-2 text-base rounded-full file:font-semibold text-violet-700 bg-white  hover:bg-amber-100 hover:cursor-pointer disabled:bg-gray-300 disabled:cursor-default",
 };
 
 const Button = ({
@@ -17,7 +19,7 @@ const Button = ({
       disabled={disable}
       onClick={handleClick}
       type={type}
-      className={`${VARIANTS[variant]} inline-flex py-2 px-6 rounded-lg text-base border w-full justify-center`}
+      className={`${VARIANTS[variant]} inline-flex py-2 px-6  text-base border w-full justify-center`}
     >
       {children}
     </button>

15b870a8bd0d8ed4015e71d374411d38fea6e0fd
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-05 22:33:01 +0700

    edited image and styles

--- src/components/common/screen/index.js
+++ src/components/common/screen/index.js
@@ -3,14 +3,8 @@ import React from "react";
 
 const ScreenLock = () => {
   return (
-    <div className="h-screen w-full flex items-center md:hidden">
-      <Image
-        src="/auth-bg.png"
-        width={425}
-        height={564}
-        alt="backdrop"
-        className="absolute top-0 h-full w-full -z-50"
-      />
+    <div className="h-screen w-full flex flex-col items-center justify-center md:hidden bg-[#4FD1C5] gap-4">
+      <Image src="/lock-illust.png" width={240} height={240} alt="cat-illust" />
       <h1 className="px-4 text-center font-bold mx-auto">
         Use Tablet or Desktop for better experiences
       </h1>

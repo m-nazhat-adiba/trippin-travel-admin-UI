271232221d08ed34eaa891745d3f6894fa8c9184
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-05 21:07:08 +0700

    changed header

--- src/utils/handleUpload.js
+++ src/utils/handleUpload.js
@@ -1,17 +1,16 @@
 import { UPLOAD_IMAGE } from "@/constant/api";
 import { IMAGE_UPLOAD_CONFIG } from "@/constant/config";
 import axios from "axios";
+import getToken from "./getToken";
 
 const handleUpload = async (file) => {
+  const header = getToken().loggedConfig;
+
   const formData = new FormData();
   formData.append("image", file);
 
   try {
-    const response = await axios.post(
-      UPLOAD_IMAGE,
-      formData,
-      IMAGE_UPLOAD_CONFIG
-    );
+    const response = await axios.post(UPLOAD_IMAGE, formData, header);
 
     console.log("File uploaded successfully:", response.data);
     return response.data;

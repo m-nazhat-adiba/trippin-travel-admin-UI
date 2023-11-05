8c8e56680ddd65ff95667926480dab45fbcce685
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-05 21:08:22 +0700

    added getToken()

--- src/apis/userService.js
+++ src/apis/userService.js
@@ -1,17 +1,41 @@
-import { USER_CONFIG, USER_LOGGED_CONFIG } from "@/constant/config";
 import useAxios from "@/hooks/useAxios";
 import { getData } from "@/utils/fetchData";
 
 import { USERS } from "../constant/api";
 
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+function getToken() {
+  let token = "";
+  if (typeof window !== "undefined") {
+    token = localStorage.getItem("token");
+  }
+  const userConfig = {
+    headers: {
+      apiKey: API_KEY,
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const loggedConfig = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      apiKey: API_KEY,
+    },
+  };
+  return { userConfig, loggedConfig };
+}
+
 function GetUserList() {
-  const userList = useAxios(getData, USERS.GET_ALL_USERS, USER_CONFIG);
+  const config = getToken().userConfig;
+  const userList = useAxios(getData, USERS.GET_ALL_USERS, config);
 
   return userList;
 }
 
 function GetLoggedUser() {
-  const userData = useAxios(getData, USERS.GET_LOGGED_USER, USER_LOGGED_CONFIG);
+  const config = getToken().loggedConfig;
+  const userData = useAxios(getData, USERS.GET_LOGGED_USER, config);
 
   return userData;
 }
